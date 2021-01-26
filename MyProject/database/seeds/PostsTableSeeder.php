1dfe2c0a9cfd8a4a23ba235c779f677b941a3b0e
Pietro <marrazzo.pietroalfonso@gmail.com>
2021-01-26 16:46:03 +0100

    seed & fakers runned successfully

--- MyProject/database/seeds/PostsTableSeeder.php
+++ MyProject/database/seeds/PostsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Post;
 use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 class PostsTableSeeder extends Seeder
 {
@@ -11,31 +12,50 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $posts = [
-            [
-                'title' => 'lorem',
-                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, saepe temporibus fugit blanditiis ad commodi non omnis facilis nisi labore laudantium? Voluptatum, veritatis ad. Maxime rem minima assumenda ut quis?',
-            ],
-            [
-                'title' => 'lorem 2',
-                'body' => 'Lorem 2, ipsum dolor sit amet consectetur adipisicing elit. Ut, saepe temporibus fugit blanditiis ad commodi non omnis facilis nisi labore laudantium? Voluptatum, veritatis ad. Maxime rem minima assumenda ut quis?',
-            ],
-            [
-                'title' => 'lorem 3',
-                'body' => 'Lorem 3, ipsum dolor sit amet consectetur adipisicing elit. Ut, saepe temporibus fugit blanditiis ad commodi non omnis facilis nisi labore laudantium? Voluptatum, veritatis ad. Maxime rem minima assumenda ut quis?',
-            ],
-        ];
+        // DECOMMENTARE SE NON SI VOGLIONO RIMUOVERE TUTTI I DATI DELLA TABELLA
+        Post::truncate();
 
-        foreach($posts as $post) {
+
+        // $posts = [
+        //     [
+        //         'title' => 'lorem',
+        //         'description' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, saepe temporibus fugit blanditiis ad commodi non omnis facilis nisi labore laudantium? Voluptatum, veritatis ad. Maxime rem minima assumenda ut quis?',
+        //     ],
+        //     [
+        //         'title' => 'lorem 2',
+        //         'description' => 'Lorem 2, ipsum dolor sit amet consectetur adipisicing elit. Ut, saepe temporibus fugit blanditiis ad commodi non omnis facilis nisi labore laudantium? Voluptatum, veritatis ad. Maxime rem minima assumenda ut quis?',
+        //     ],
+        //     [
+        //         'title' => 'lorem 3',
+        //         'description' => 'Lorem 3, ipsum dolor sit amet consectetur adipisicing elit. Ut, saepe temporibus fugit blanditiis ad commodi non omnis facilis nisi labore laudantium? Voluptatum, veritatis ad. Maxime rem minima assumenda ut quis?',
+        //     ],
+        // ];
+        
+        for ($i=0; $i < 10; $i++) {
+            $title = $faker->text(50);
+
             $newPost = new Post();
 
-            $newPost->title = $post['title'];
-            $newPost->body = $post['body'];
-            // $newPost->slug = Str::slug($post['title'], '-');
+            $newPost->title = $title;
+            $newPost->description = $faker->paragraph(2, true);
+            $newPost->slug = Str::slug($title, '-');
 
             $newPost->save();
-        } 
+
+        }
+
+
+
+        // foreach($posts as $post) {
+        //     $newPost = new Post();
+
+        //     $newPost->title = $post['title'];
+        //     $newPost->description = $post['description'];
+        //     $newPost->slug = Str::slug($post['title'], '-');
+
+        //     $newPost->save();
+        // } 
     }
 }
